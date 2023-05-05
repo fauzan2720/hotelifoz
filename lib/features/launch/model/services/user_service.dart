a366d206fa1a7a366a268c81ed4aeeca38e2a40a
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-05-05 07:03:48 +0700

    update service login firebase auth (#17)

--- lib/features/launch/model/services/user_service.dart
+++ lib/features/launch/model/services/user_service.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hotelifoz/features/launch/model/models/user_model.dart';
@@ -8,35 +9,54 @@ class UserService {
           .collection("users")
           .doc(FirebaseAuth.instance.currentUser!.uid);
 
-  Stream<UserModel> getUser() {
+  Stream<Either<String, UserModel>> getUser() {
     try {
       return _userCollection.snapshots().map((event) {
         final UserModel result =
             UserModel.fromMap(event.data() as Map<String, dynamic>);
-        return result;
+        return Right(result);
       });
     } catch (e) {
-      throw Exception(e);
+      throw Left(e.toString());
     }
   }
 
-  Future<void> createUserIfNotExists() async {
+  Future<Either<String, bool>> createUserIfNotExists() async {
     final User currentUser = FirebaseAuth.instance.currentUser!;
     final DocumentSnapshot<Map<String, dynamic>> snapshot =
         await _userCollection.get();
-    final String displayName = currentUser.email!.replaceAll("@gmail.com", "");
+    final String displayName =
+        currentUser.displayName ?? _convertEmailToName(currentUser.email!);
 
-    if (!snapshot.exists) {
-      await _userCollection.set(UserModel(
-        id: currentUser.uid,
-        name: displayName,
-        email: currentUser.email!,
-        phoneNumber: currentUser.phoneNumber,
-        photo: currentUser.photoURL ??
-            "https://ui-avatars.com/api/?name=$displayName",
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now(),
-      ).toMap());
+    try {
+      if (!snapshot.exists) {
+        await _userCollection.set(UserModel(
+          id: currentUser.uid,
+          name: displayName,
+          email: currentUser.email!,
+          phoneNumber: currentUser.phoneNumber,
+          photo: currentUser.photoURL ??
+              "https://ui-avatars.com/api/?name=$displayName",
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+        ).toMap());
+        return const Right(true);
+      } else {
+        return const Right(true);
+      }
+    } catch (e) {
+      return Left(e.toString());
     }
   }
+
+  String _convertEmailToName(String email) => email
+      .split("@")[0] // menghilangkan email
+      .replaceAll(".", " ")
+      .replaceAll("_", " ")
+      .replaceAll(RegExp(r'\d+'), '') // menghapus digit
+      .toLowerCase() // mengubah semua huruf menjadi lowercase
+      .split(' ') // memecah nama menjadi array
+      .map((e) =>
+          e[0].toUpperCase() + e.substring(1)) // membuat awalan huruf kapital
+      .join(' '); // menggabungkan array kembali menjadi string;
 }
--- test/features/launch/model/services/user_service_test.mocks.dart
+++ test/features/launch/model/services/user_service_test.mocks.dart
@@ -3,11 +3,12 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i3;
+import 'dart:async' as _i4;
 
-import 'package:hotelifoz/features/launch/model/models/user_model.dart' as _i4;
+import 'package:dartz/dartz.dart' as _i2;
+import 'package:hotelifoz/features/launch/model/models/user_model.dart' as _i5;
 import 'package:hotelifoz/features/launch/model/services/user_service.dart'
-    as _i2;
+    as _i3;
 import 'package:mockito/mockito.dart' as _i1;
 
 // ignore_for_file: type=lint
@@ -21,29 +22,47 @@ import 'package:mockito/mockito.dart' as _i1;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
+class _FakeEither_0<L, R> extends _i1.SmartFake implements _i2.Either<L, R> {
+  _FakeEither_0(
+    Object parent,
+    Invocation parentInvocation,
+  ) : super(
+          parent,
+          parentInvocation,
+        );
+}
+
 /// A class which mocks [UserService].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockUserService extends _i1.Mock implements _i2.UserService {
+class MockUserService extends _i1.Mock implements _i3.UserService {
   MockUserService() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i3.Stream<_i4.UserModel> getUser() => (super.noSuchMethod(
+  _i4.Stream<_i2.Either<String, _i5.UserModel>> getUser() =>
+      (super.noSuchMethod(
         Invocation.method(
           #getUser,
           [],
         ),
-        returnValue: _i3.Stream<_i4.UserModel>.empty(),
-      ) as _i3.Stream<_i4.UserModel>);
+        returnValue: _i4.Stream<_i2.Either<String, _i5.UserModel>>.empty(),
+      ) as _i4.Stream<_i2.Either<String, _i5.UserModel>>);
   @override
-  _i3.Future<void> createUserIfNotExists() => (super.noSuchMethod(
+  _i4.Future<_i2.Either<String, bool>> createUserIfNotExists() =>
+      (super.noSuchMethod(
         Invocation.method(
           #createUserIfNotExists,
           [],
         ),
-        returnValue: _i3.Future<void>.value(),
-        returnValueForMissingStub: _i3.Future<void>.value(),
-      ) as _i3.Future<void>);
+        returnValue: _i4.Future<_i2.Either<String, bool>>.value(
+            _FakeEither_0<String, bool>(
+          this,
+          Invocation.method(
+            #createUserIfNotExists,
+            [],
+          ),
+        )),
+      ) as _i4.Future<_i2.Either<String, bool>>);
 }
